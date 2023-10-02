78ded2e7981c6c3e195f06c59d6d561a674c250c
SANJANA <sanjana.js02@gmail.com>
2023-10-02 19:54:08 +0530

    done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -17,10 +17,10 @@ function HomePage() {
     }, [])
     return (
         <>
-        <h1 className="h1 display-6">WikiCountries: Your Guide to the World</h1>
-            <ul className='list-unstyled list-group container d-flex align-items-md-center '>
+        <h1>WikiCountries: Your Guide to the World</h1>
+            <ul>
                 {countries.map(country => (
-                    <li className='list-group-item list-group-item-action w-50' key={country._id}>
+                    <li key={country._id}>
                         <Link to={`/${country.alpha3Code}`}>
                             <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} />
                             <h4 >{country.name.official}</h4>
